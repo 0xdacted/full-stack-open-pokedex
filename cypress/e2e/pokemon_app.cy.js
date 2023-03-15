c0fd3f70acf719235d67b30d64b7345cef85ece0
Red <crabmarket56@gmail.com>
2023-03-15 11:28:30 +0900

    Add navigation test

--- cypress/e2e/pokemon_app.cy.js
+++ cypress/e2e/pokemon_app.cy.js
@@ -5,4 +5,9 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('can navigate to ivysaur page', function() {
+    cy.visit('http://localhost:4000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
